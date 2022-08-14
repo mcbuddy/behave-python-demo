cbaf3c9f31192978e4d01f0526a80218c7cce308
Budi Sugianto <budisugianto777@gmail.com>
2022-08-14 00:30:32 -0400

    trying

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
 
 def before_scenario(context, scenario):
-    context.browser = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--disable-gpu") 
+    if os.getenv('HEADLESS') == "true":
+        options.add_argument("--headless") 
+
+    context.browser = webdriver.Chrome(options=options)
     context.browser.maximize_window()
 
 
