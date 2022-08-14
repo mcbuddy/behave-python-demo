2c59c6319effb28f9cdaee104bfc25c92de7f6b8
Budi Sugianto <budisugianto777@gmail.com>
2022-08-14 00:39:08 -0400

    Fix the github-action issue

--- features/environment.py
+++ features/environment.py
@@ -4,9 +4,9 @@ import os
 
 def before_scenario(context, scenario):
     options = Options()
-    options.add_argument("--disable-gpu") 
-    if os.getenv('HEADLESS') == "true":
-        options.add_argument("--headless") 
+    options.add_argument("disable-gpu")
+    if os.getenv("HEADLESS") == 'true':
+      options.add_argument("headless")
 
     context.browser = webdriver.Chrome(options=options)
     context.browser.maximize_window()
